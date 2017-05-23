c585bb81df7d1185a13a68ce31f3147509d98aab
studio <eli.magana@imaginaestudio.mx>
2017-05-23 16:57:25 -0500

    index logic system complete 2

--- index.php
+++ index.php
@@ -35,58 +35,9 @@
 
 </head>
 <body>
-<header>
-    <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand page-scroll" href="."><img src="images/logo.png"  class="img-logo" alt="" width="230" height="38"></a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav">
-                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
-                    <li class="hidden">
-                        <a href="#page-top"></a>
-                    </li>
-                    <li class="active">
-                        <a href=".">Inicio</a>
-                    </li>
-                    <li>
-                        <a href="">Productos</a>
-                    </li>
-                    <li>
-                        <a href="">Servicios</a>
-                    </li>
-                    <li>
-                        <a href="">Casos de éxito</a>
-                    </li>
-                    <li>
-                        <a href="">Descargas</a>
-                    </li>
-                    <li>
-                        <a href="">Nosotros</a>
-                    </li>
-                    <li>
-                        <a href="#contacto">Contacto</a>
-                    </li>
-                    <li>
-                        <a href="" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-</header>
+<?php
+include_once('layouts/partial/header.php');
+?>
 
 <!--======= SLIDER =========-->
 <section id="slider-home">
@@ -270,100 +221,9 @@
     </div>
 </section>
 
-<section id="footer">
-    <div class="container">
-        <div class="row">
-            <div class="tittle-contact">
-                <h1 class="">Contáctanos</h1>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-8">
-                <div class="contact-form">
-                    <div class="row">
-                        <div class="contactForm">
-                            <!-- Start Contact Form -->
-                            <form id="contactForm" action="#" method="post" role="form">
-                                <div class="col-xs-12 col-sm-4 col-md-4">
-                                    <div class="form-group">
-                                        <input id="formName" type="text" class="input required" name="nombre" placeholder="* Nombre" required>
-                                    </div>
-                                </div>
-                                <div class="col-xs-12 col-sm-4 col-md-4">
-                                    <div class="form-group">
-                                        <input id="phone" type="text" class="input phone required" placeholder="* Teléfono" maxlength="10" size="30" name="telefono" title="Escribe un teléfono válido con 10 dígitos" required>
-                                    </div>
-                                </div>
-                                <div class="col-xs-12 col-sm-4 col-md-4">
-                                    <div class="form-group">
-                                        <input id="formEmail" type="email" class="input email required" name="email" placeholder="* Correo Electrónico"  required>
-                                    </div>
-                                </div>
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <textarea id="message" class="textarea" name="message" rows="5" cols="40" placeholder="Comentarios"></textarea>
-                                    </div>
-                                </div>
-                                <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
-                                    <div class="col-xs-12 col-sm-7 col-md-7">
-                                        <div class="form-group">
-                                            <div class="captcha">
-                                                <div id="recaptchaFooter"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-xs-12 col-sm-5 col-md-5">
-                                        <div class="form-group">
-                                            <button value="Submit" id="contactForm_submit_contacto" class="btn_frm_contact center-block" type="submit">ENVIAR</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                            <div class="successMsg"></div>
-                            <!-- End Contact Form -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-4 col-md-4">
-                <div class="row info">
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <a href="tel:9999280005"> <i class="fa fa-phone-square fa-fw" aria-hidden="true"></i> &nbsp;(999) 928-0005</a>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <a href="mailto:info@logicsystems.com.mx"><i class="fa fa-envelope fa-fw" aria-hidden="true"></i> &nbsp;info@logicsystems.com.mx</a>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <a href="javascript:;">
-                            <table>
-                                <tr>
-                                    <td><i class="fa fa-map-marker fa-fw" aria-hidden="true"></i> &nbsp;</td>
-                                    <td>
-                                        Calle 70 #422 por 45 y 47, Centro, C.P. 97000<br>
-                                        Mérida, Yucatán, México.
-                                    </td>
-                                </tr>
-                            </table>
-                        </a>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <a href=""><i class="fa fa-facebook-square fa-fw" aria-hidden="true"></i> &nbsp;LogicSystemsMX</a>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="logo-footer">
-                            <img src="images/logo_footer.png" class="img-responsive">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="copyright">
-                <p>© Copyright 2017 Logic Systems. Todos los derechos reservados. |  <a href="">Aviso de privacidad</a>.</p>
-            </div>
-        </div>
-    </div>
-</section>
+<?php
+include_once('layouts/partial/footer.php');
+?>
 
 
 
